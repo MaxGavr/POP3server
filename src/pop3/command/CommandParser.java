2c93e8b411edfec194397b48e4cb37bb56521dc7
Max Gavrilyuk <max.gavr@tut.by>
2018-02-11 16:40:56 +0300

    Remove unnecessary code

--- src/pop3/command/CommandParser.java
+++ src/pop3/command/CommandParser.java
@@ -3,7 +3,6 @@ package pop3.command;
 import pop3.command.POP3Response;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,18 +15,7 @@ public class CommandParser {
 	
 	// TODO: add \r\n
 	private static Pattern mPOP3CommandPattern = Pattern.compile("^[\\p{Print}]{3,4}( [\\p{Print}]{1,40})*$");
-	private static LinkedList<String> mKnownCommands = new LinkedList<String>();
-	static {
-		mKnownCommands.add("USER");
-		mKnownCommands.add("PASS");
-		mKnownCommands.add("QUIT");
-		mKnownCommands.add("STAT");
-		mKnownCommands.add("LIST");
-		mKnownCommands.add("RETR");
-		mKnownCommands.add("DELE");
-		mKnownCommands.add("NOOP");
-	}
-	
+
 	
 	public static boolean validate(String command) {
 		if (command == null) {
